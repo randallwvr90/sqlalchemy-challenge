ebf4ae5a89a3006372cd434708d9cb769bc5cb5d
Randy Weaver <randallwvr90@gmail.com>
2021-12-24 23:25:57 -0600

    clean up app.py and update gitignore

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ Base = automap_base()
 Base.prepare(engine, reflect = True)
 
 #Save the refrences to the tables
-Measurement = Base.classes.measurement
-Station = Base.classes.station
+measurements = Base.classes.measurement
+theStations = Base.classes.station
 mostRecentDate = ""
 
 ########################## Flask ##############################
@@ -34,8 +34,7 @@ app = Flask(__name__)
 def index():
 
     return (
-        f'<h3>Available Routes for this Climate App</h3><br>'
-
+        f'<h1>Available Routes for this Climate App</h1><br>'
         f'<ul><li> /api/v1.0/precipitation </li><br>'
         f'<li> /api/v1.0/stations </li><br>'
         f'<li> /api/v1.0/tobs </li><br>'
@@ -43,15 +42,130 @@ def index():
         f'<li> /api/v1.0/<start>/<end> </li></ul><br>'
         )
 
-
-
-
 ############## Route - Precipitation by Date ##################
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+    # Start the session
+    session = Session(engine)
 
-############### Route - Display all Stations ##################
+    # Find the most recent date and the date one year earlier
+    latestDate = session.query(measurements.date).order_by(measurements.date.desc()).first()
+    queryDate = dt.date.fromisoformat(latestDate.date) - dt.timedelta(days=365)
 
-########### Route - Temp Data for Most Active Station #########
+    # Query for the precipitation data
+    precipData = session.query(measurements.prcp, measurements.date).\
+        filter(measurements.date > queryDate).order_by(measurements.date.desc())
 
-############# Route - Temp Data for Given Start Date ##########
+    session.close()
 
-############# Route - Temp Data for Given Date Range ##########
+    # Create dictionary, return jsonified version
+    precipList = []
+    for dataPoint, date in precipData:
+        precipDict = {
+            'Date': date,
+            'Precipitation': dataPoint
+        }
+        precipList.append(precipDict)
+    return jsonify(precipList)
+
+############### Route - Display all stationss ##################
+@app.route('/api/v1.0/stations')
+def stations():
+    # Start the session
+    session = Session(engine)
+
+    # Query for the stations
+    stationsList = [] 
+    theseStations = session.query(theStations.station,theStations.name).distinct().all()
+
+    session.close()
+
+    for station in theseStations:
+        stationDict = {
+            'Station ID': station.station,
+            'Station Name': station.name
+        }
+        stationsList.append(stationDict)
+    return jsonify(stationsList)
+
+########### Route - Temp Data for Most Active stations #########
+@app.route('/api/v1.0/tobs')
+def tobs():
+    # Start the session
+    session = Session(engine)
+
+    # Query to find the most active station
+    aCount = func.count('*').label('count')
+    stationsByActivity = session.query(aCount, measurements.station).\
+        group_by(measurements.station).order_by(aCount.desc())
+    mostActiveStation = stationsByActivity.all()[0][1]
+
+    # Query for the most active station's temp data for the previous year
+    # Find the most recent date and the date one year earlier
+    latestDate = session.query(measurements.date).order_by(measurements.date.desc()).first()
+    queryDate = dt.date.fromisoformat(latestDate.date) - dt.timedelta(days=365)
+    activeStationTempData = session.query(measurements.date, measurements.tobs).\
+        filter(measurements.station == mostActiveStation).\
+        filter(measurements.date > queryDate).all()
+    
+    session.close()
+
+    tobsList = []
+    for date, tobs in activeStationTempData:
+        tobsDict = {
+            'Date': date,
+            'Temperature': tobs
+        }
+        tobsList.append(tobsDict)
+
+    return jsonify(tobsList)
+
+############# Route - Temp Data for Given Start Date ###########
+@app.route("/api/v1.0/<start>")
+def start_temp(start):
+    # Create the session
+    session = Session(engine)
+
+    # set up min avg and max functions
+    selection = [func.min(measurements.tobs), func.avg(measurements.tobs), func.max(measurements.tobs)]
+
+    # Query for the temperatures since the start date
+    tempInfo = session.query(*selection).filter(measurements.date >= start).all()
+    tempInfoList = list(np.ravel(tempInfo))
+
+    session.close()
+
+    tempInfoDict = {
+        'Minimum Observed Temperature': tempInfoList[0],
+        'Average Observed Temperature': round(tempInfoList[1], 1),
+        'Maximum Observed Temperature': tempInfoList[2]
+    }
+    
+    return jsonify(tempInfoDict)
+
+############# Route - Temp Data for Given Date Range ###########
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_temp(start, end):
+    # Create the session
+    session = Session(engine)
+
+    # set up min avg and max functions
+    selection = [func.min(measurements.tobs), func.avg(measurements.tobs), func.max(measurements.tobs)]
+
+    # Query for the temperatures since the start date
+    tempInfo = session.query(*selection).filter(measurements.date >= start).filter(measurements.date <= end).all()
+    tempInfoList = list(np.ravel(tempInfo))
+
+    session.close()
+
+    tempInfoDict = {
+        'Minimum Observed Temperature': tempInfoList[0],
+        'Average Observed Temperature': round(tempInfoList[1], 1),
+        'Maximum Observed Temperature': tempInfoList[2]
+    }
+    
+    return jsonify(tempInfoDict)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
